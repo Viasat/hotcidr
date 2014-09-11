bbf7107432d585b8cf906b3583134404c7133252
Justin Bass <justin.bass@viasat.com>
2014-09-11 12:14:14 -0700

    Added seconds_to_wait for flexible testing, and fixed problem with the test itself

--- HotCIDR/hotcidr/test/test_expiration.py
+++ HotCIDR/hotcidr/test/test_expiration.py
@@ -5,12 +5,13 @@ import git
 import yaml
 import tempfile
 
-import hotcidr.util
+from hotcidr import util
 import hotcidr.fetchvpc
 import hotcidr.deleteexpired
 
 test_repo_name = 'repo_test'
 test_repo_src = os.path.join(os.path.realpath(__file__).rsplit('/',1)[0], test_repo_name)
+seconds_to_wait = 5
 
 class TestExpiration(unittest.TestCase):
     def test_rulesyaml_expiration(self):
@@ -33,8 +34,8 @@ class TestExpiration(unittest.TestCase):
         yaml_file = yaml.load(test_file)
         test_file.close()
 
-        yaml_file['rules'][0]['expiration'] = 2
-        yaml_file['rules'][1]['expiration'] = 4
+        yaml_file['rules'][0]['expiration'] = seconds_to_wait 
+        yaml_file['rules'][1]['expiration'] = 2*seconds_to_wait 
 
         test_file = open(test_file_path, 'w')
         test_file.write(yaml.dump(yaml_file, default_flow_style=False))
@@ -54,7 +55,7 @@ class TestExpiration(unittest.TestCase):
         self.assertEqual(len(yaml_pre['rules']), 2)
 
         #Wait 2 total seconds for the rule to expire, then delete it
-        time_to_wait = 3
+        time_to_wait = seconds_to_wait*3/2 #Allow some buffer time for deleteexpire to run
         if time.time() - start_time < time_to_wait:
             sleeptime = time_to_wait - (time.time() - start_time)
         else:
@@ -71,7 +72,7 @@ class TestExpiration(unittest.TestCase):
         self.assertEqual(len(yaml_post['rules']), 1)
 
         #Wait 4 total seconds for the rule to expire, then delete it
-        time_to_wait = 5
+        time_to_wait = 2*seconds_to_wait*3/2
         if time.time() - start_time < time_to_wait:
             sleeptime = time_to_wait - (time.time() - start_time)
         else:
@@ -86,7 +87,7 @@ class TestExpiration(unittest.TestCase):
         self.assertEqual(len(yaml_none['rules']), 0)
 
         #Clean up temp directory
-        temp_path.close()
+        rmtree(temp_path)
 
     def test_expirationyaml(self):
         #Create new repo from repo_test
@@ -107,7 +108,7 @@ class TestExpiration(unittest.TestCase):
         #Make the actual expiration change and commit it
         test_file = open(expirations_file_path, 'w')
 
-        test_file_yaml = yaml.dump({'rules':[{'location':'192.168.0.2','ports':443,'expiration':2},{'location':'192.168.0.3','ports':443,'expiration':4}]}, default_flow_style=False)
+        test_file_yaml = yaml.dump({'rules':[{'location':'192.168.0.2/32','ports':443,'expiration':seconds_to_wait},{'location':'192.168.0.3/32','ports':443,'expiration':2*seconds_to_wait}]}, default_flow_style=False)
 
         test_file.write(test_file_yaml)
         test_file.close()
@@ -127,7 +128,7 @@ class TestExpiration(unittest.TestCase):
         self.assertEqual(len(yaml_pre['rules']), 2)
 
         #Wait 2 total seconds for the rule to expire, then delete it
-        time_to_wait = 3
+        time_to_wait = seconds_to_wait*3/2
         if time.time() - start_time < time_to_wait:
             sleeptime = time_to_wait - (time.time() - start_time)
         else:
@@ -142,7 +143,7 @@ class TestExpiration(unittest.TestCase):
         self.assertEqual(len(yaml_post['rules']), 1)
 
         #Wait 4 total seconds for last rule to expire, then delete it
-        time_to_wait = 5
+        time_to_wait = 2*seconds_to_wait*3/2
         if time.time() - start_time < time_to_wait:
             sleeptime = time_to_wait - (time.time() - start_time)
         else:
@@ -157,7 +158,7 @@ class TestExpiration(unittest.TestCase):
         self.assertEqual(len(yaml_none['rules']), 0)
 
         #Clean up temp directory
-        rmtree(GIT_REPO_DIR)
+        rmtree(temp_path)
 
 if __name__ == '__main__':
     unittest.main()
