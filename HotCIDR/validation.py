d6e4c7da13debc1d4aac6f6fe2360f6ef48e9cc7
Stephan Kemper <stephan.kemper@viasat.com>
2015-05-14 19:26:14 -0700

    Flatten file structure

--- HotCIDR/validation.py
+++ HotCIDR/validation.py
@@ -9,6 +9,7 @@ import toposort
 import yaml
 import hotcidr.state
 
+
 def inflect_a(s, p=inflect.engine()):
     x = p.plural(s)
     if p.compare(s, x) == 'p:s':
@@ -17,6 +18,7 @@ def inflect_a(s, p=inflect.engine()):
 
 logging.basicConfig(format='%(levelname)s %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p')
+
 
 class Validator(object):
     logger = logging.getLogger('validation')
@@ -48,19 +50,16 @@ class Validator(object):
             self.register_check(f)
 
     required_map = {}
+
     def validate(self, wrap=True):
         # TODO: spawn multiple processes
-        l = {f: Validator.required_map[f]
-                 if f in Validator.required_map
-                 else set()
-                 for f in self.checks}
+        l = {f: Validator.required_map[f] if f in Validator.required_map else set() for f in self.checks}
         for f in toposort.toposort_flatten(l, sort=False):
             if wrap:
                 try:
                     f(self)
                 except:
-                    self.fatal("Unexpected exception raised by %s" %
-                        f.__name__)
+                    self.fatal("Unexpected exception raised by %s" % f.__name__)
                     raise
             else:
                 f(self)
@@ -79,11 +78,13 @@ def has_rules(g):
         elif 'rules' in i:
             yield i
 
+
 def requires(*a):
     def decorator(f):
         Validator.required_map[f] = set(a)
         return f
     return decorator
+
 
 def load_groups(self, forced=False):
     if forced or not hasattr(self, 'groups'):
@@ -95,22 +96,23 @@ def load_groups(self, forced=False):
                 if x.endswith('.yaml'):
                     self.groups[x[:-5]] = self.load(os.path.join('groups', x))
 
+
 def load_boxes(self, forced=False):
     if forced or not hasattr(self, 'boxes'):
-      self.boxes = self.load('boxes.yaml')
+        self.boxes = self.load('boxes.yaml')
+
 
 @requires(load_groups, load_boxes)
 def find_unused_groups(self):
-    #TODO: include groups used in 'location' field
-    used = set(itertools.chain(*(b['groups'] for b in self.boxes.values()
-                                             if 'groups' in b)))
+    # TODO: include groups used in 'location' field
+    used = set(itertools.chain(*(b['groups'] for b in self.boxes.values() if 'groups' in b)))
     for g in set(self.groups.keys()) - used:
         self.info("Group %s is unused" % g)
 
+
 @requires(load_groups, load_boxes)
 def validate_groups(self):
-    used = set(itertools.chain(*(b['groups'] for b in self.boxes.values()
-                                             if 'groups' in b)))
+    used = set(itertools.chain(*(b['groups'] for b in self.boxes.values() if 'groups' in b)))
     valid_groups = set(self.groups.keys())
     for g in used - valid_groups:
         guess = difflib.get_close_matches(g, valid_groups)
@@ -120,6 +122,7 @@ def validate_groups(self):
             guess = ""
 
         self.fatal("%s is not defined%s" % (g, guess))
+
 
 @requires(load_groups)
 def validate_group_names(self):
@@ -133,11 +136,13 @@ def validate_group_names(self):
         if any(c not in valid_chars for c in name):
             self.fatal("%s is not a valid group name" % name)
 
+
 @requires(load_boxes)
 def validate_aws_instance_id(self):
     for name in self.boxes.keys():
         if not re.match(r'^i\-[0-9a-f]{8}$', name):
             self.fatal("Instance ID %s is not a valid AWS instance ID" % name)
+
 
 @requires(load_groups)
 def validate_aws_group_id(self):
@@ -155,20 +160,20 @@ def validate_aws_group_id(self):
             else:
                 seen[name] = group_name
 
+
 @requires(load_groups)
 def validate_protocols(self):
     for group_name, group in has_rules(self.groups.iteritems()):
         for rule_num, rule in enumerate(group['rules'], 1):
             if 'protocol' not in rule:
-                self.error("Rule %d in %s is missing a protocol" %
-                    (rule_num, group_name))
+                self.error("Rule %d in %s is missing a protocol" % (rule_num, group_name))
             elif rule['protocol'] == '-1':
-                self.error("Rule %d in %s has an invalid protocol" %
-                    (rule_num, group_name))
+                self.error("Rule %d in %s has an invalid protocol" % (rule_num, group_name))
+
 
 @requires(load_groups)
 def validate_ports(self):
-    #TODO: handle ICMP fromport
+    # TODO: handle ICMP fromport
     def port(x, default=-1):
         try:
             r = int(x)
@@ -181,13 +186,11 @@ def validate_ports(self):
             valid = True
 
             if 'fromport' not in rule:
-                self.error("Rule %d in %s is missing a fromport" %
-                    (rule_num, group_name))
+                self.error("Rule %d in %s is missing a fromport" % (rule_num, group_name))
                 valid = False
 
             if 'toport' not in rule:
-                self.error("Rule %d in %s is missing a toport" %
-                    (rule_num, group_name))
+                self.error("Rule %d in %s is missing a toport" % (rule_num, group_name))
                 valid = False
 
             if valid:
@@ -195,22 +198,19 @@ def validate_ports(self):
                 toport = port(rule['toport'])
                 valid = True
                 if not fromport:
-                    self.error("Rule %d in %s has an invalid fromport" %
-                        (rule_num, group_name))
+                    self.error("Rule %d in %s has an invalid fromport" % (rule_num, group_name))
                     valid = False
 
                 if not toport:
-                    self.error("Rule %d in %s has an invalid toport" %
-                        (rule_num, group_name))
+                    self.error("Rule %d in %s has an invalid toport" % (rule_num, group_name))
                     valid = False
 
                 if valid:
                     if fromport > toport:
-                        self.error("Rule %d in %s has an invalid port range" %
-                            (rule_num, group_name))
+                        self.error("Rule %d in %s has an invalid port range" % (rule_num, group_name))
                     elif (toport - fromport) >= 100:
-                        self.warn("Rule %d in %s has a large port range" %
-                            (rule_num, group_name))
+                        self.warn("Rule %d in %s has a large port range" % (rule_num, group_name))
+
 
 @requires(load_groups)
 def validate_rule_fields(self):
@@ -218,8 +218,10 @@ def validate_rule_fields(self):
         for rule_num, rule in enumerate(group['rules'], 1):
             for field in ('description',):
                 if field not in rule:
-                    self.warn("Rule %d in %s is missing %s" %
-                        (rule_num, group_name, inflect_a(field)))
+                    self.warn("Rule %d in %s is missing %s" % (rule_num, group_name, inflect_a(field)))
+                elif rule[field] == '' or rule[field] is None:
+                    self.warn("Rule %d in %s has an empty %s" % (rule_num, group_name, field))
+
 
 @requires(load_groups)
 def validate_group_fields(self):
@@ -228,13 +230,14 @@ def validate_group_fields(self):
             if field not in group:
                 self.warn("%s is missing %s" % (group_name, inflect_a(field)))
 
+
 @requires(load_boxes)
 def validate_instance_fields(self):
     for box_id, box in self.boxes.iteritems():
         for field in ('ip', 'domain', 'groups'):
             if field not in box:
-                self.warn("Box %s is missing %s" %
-                    (box_id, inflect_a(field)))
+                self.warn("Box %s is missing %s" % (box_id, inflect_a(field)))
+
 
 @requires(load_groups)
 def validate_locations(self):
@@ -247,11 +250,8 @@ def validate_locations(self):
                         ip = netaddr.IPNetwork(rule['location'])
                         if str(ip.cidr) != rule['location']:
                             self.warn("Location for rule %d in %s "
-                                      "will be interpreted as %s" %
-                                (rule_num, group_name, ip.cidr))
+                                      "will be interpreted as %s" % (rule_num, group_name, ip.cidr))
                     except netaddr.AddrFormatError:
-                        self.error("Rule %d in %s has an invalid location" %
-                            (rule_num, group_name))
+                        self.error("Rule %d in %s has an invalid location" % (rule_num, group_name))
             else:
-                self.error("Rule %d in %s is missing a location" %
-                    (rule_num, group_name))
+                self.error("Rule %d in %s is missing a location" % (rule_num, group_name))
